19019cb8fa4631abb56fd007e0153ed860c5217e
nmariano <mariano.nick@gmail.com>
2018-02-07 21:41:12 -0600

    Playing around with local directories instead of hard coded file paths

--- DailyProgrammer349/Program.cs
+++ DailyProgrammer349/Program.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace DailyProgrammer349
 {
@@ -10,6 +11,8 @@ namespace DailyProgrammer349
         static void Main(string[] args)
         {
             //var ih = new InputHandler("3 34312332");
+
+            var test = Directory.GetCurrentDirectory() + @"\input.txt";  // is the working directory of the compiled exe (in bin\debug)
             var fileInputReader = new FileInputGetter(@"C:\Users\Nick\source\repos\DailyProgrammer349\DailyProgrammer349\input.txt");
             var ih = new InputHandler(fileInputReader.GetInput());
             var bsvc = new BoxStackingViabilityChecker(ih);
